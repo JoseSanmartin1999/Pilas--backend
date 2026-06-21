7d3e4b83bc0537869057c07e261560113b776091
Programado1plux <programador1@plux.ec>
2026-06-21 15:10:37 -0500

    fix: cambie el orden de importacion de dotenv para evitar hoisting de imports

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
+import 'dotenv/config';
 import app from './app.js';
-import dotenv from 'dotenv';
 import http from 'http';
 import { Server } from 'socket.io';
 import { registerChatSocket } from './sockets/chatSocket.js';
@@ -7,8 +7,6 @@ import { initReminderScheduler } from './services/reminderScheduler.js';
 
 import db from './config/db.js';
 import connectMongo from './config/mongo.js';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
